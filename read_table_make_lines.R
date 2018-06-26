69cac8c57313e7a4a00aeffec09a80cdccba39f3
George Girton <george.girton@gmail.com>
2018-06-26 16:18:44 -0700

    initial commit: Read export distance table Write .pln file with lines from the location on one layer, to each of the locations on the other layer

---
+++ read_table_make_lines.R
@@ -0,0 +1,53 @@
+library(tidyverse)
+library(magrittr)
+library(readxl)
+library(glue)
+
+distance_table <- read_excel("HarborGarden-to-WF.xlsx")
+# some grids had pound keys & will have to prepend later
+
+# Only keep the destination location key, the origion location (MicroGrid) key, and the distance field
+distances <- distance_table %>% 
+  transmute(key = `key_from:From key`, 
+            k_to = `key_to:To key`,
+            location_name = `name_from:From name`,
+            name = `name_to:To name`,
+            from_lat = `lat_from:From latitude (NAD83)`,
+            from_long = `lng_from:From longitude (NAD83)`,
+            to_lat = `lat_to:To latitude (NAD83)`,
+            to_long = `lng_to:To longitude (NAD83)`,
+            distance = `dist_mi:Distance (miles)`) %>%
+  arrange(k_to) 
+
+length(unique(distances$key)) == 1
+length(unique(distances$k_to)) == 7  ## check to see if it matches the number of distance lines, in this case 7
+
+
+
+# Form output polyline text file lines ------------------------------------
+
+distances$writeline <- 
+  glue('\n\n{distances$key} "{distances$distance} miles"\n<1>{distances$from_lat} {distances$from_long},{distances$to_lat} {distances$to_long}')
+outputlines <- glue::collapse(distances$writeline)
+
+
+# Output to file ----------------------------------------------------------
+
+outputname <- c("spidergram.pln")
+write_file("{NAD83}",outputname)
+write_file(outputlines,outputname, append = TRUE)
+
+
+# Reference: Write a file that looks like this: --------------------------------------
+
+# {NAD83}
+# Line0001 "Line 0001"
+# <1>33.77279 117.92135,33.73294 117.99030
+# Line0002 "Line 0002"
+# <1>33.77077 117.92018,33.62331 117.87298
+# Line0003 "Line 0003"
+# <1>33.77331 117.91712,33.70163 117.82585
+# Line0004 "Line 0004"
+# <1>33.77637 117.91953,33.66628 117.75566
+# Line0005 "Line 0005"
+# <1>33.78040 117.92383,33.90514 117.85065
