8a7a5f35371eb5b15883d2d720a88b569d9bd32e
George Girton <george.girton@gmail.com>
2018-06-26 20:15:44 -0700

    Added 'rounding' of distances, Lat/Long, to reasonable display decimals.

--- read_table_make_lines.R
+++ read_table_make_lines.R
@@ -1,21 +1,19 @@
 library(tidyverse)
-library(magrittr)
 library(readxl)
 library(glue)
 
 distance_table <- read_excel("HarborGarden-to-WF.xlsx")
-# some grids had pound keys & will have to prepend later
 
-# Only keep the destination location key, the origion location (MicroGrid) key, and the distance field
+# Just keep the indicated fields
 distances <- distance_table %>% 
   transmute(key = `key_from:From key`, 
             k_to = `key_to:To key`,
             location_name = `name_from:From name`,
             name = `name_to:To name`,
-            from_lat = `lat_from:From latitude (NAD83)`,
-            from_long = `lng_from:From longitude (NAD83)`,
-            to_lat = `lat_to:To latitude (NAD83)`,
-            to_long = `lng_to:To longitude (NAD83)`,
+            from_lat = round(`lat_from:From latitude (NAD83)`,6),
+            from_long = round(`lng_from:From longitude (NAD83)`,6),
+            to_lat = round(`lat_to:To latitude (NAD83)`,6),
+            to_long = round(`lng_to:To longitude (NAD83)`,6),
             distance = `dist_mi:Distance (miles)`) %>%
   arrange(k_to) 
 
@@ -27,7 +25,7 @@ length(unique(distances$k_to)) == 7  ## check to see if it matches the number of
 # Form output polyline text file lines ------------------------------------
 
 distances$writeline <- 
-  glue('\n\n{distances$key} "{distances$distance} miles"\n<1>{distances$from_lat} {distances$from_long},{distances$to_lat} {distances$to_long}')
+  glue('\n\n{distances$key} "{round(distances$distance,2)} miles"\n<1>{distances$from_lat} {distances$from_long},{distances$to_lat} {distances$to_long}')
 outputlines <- glue::collapse(distances$writeline)
 
 
@@ -51,3 +49,4 @@ write_file(outputlines,outputname, append = TRUE)
 # <1>33.77637 117.91953,33.66628 117.75566
 # Line0005 "Line 0005"
 # <1>33.78040 117.92383,33.90514 117.85065
+
